bdab11a96d427b47970830ecf7737303d815d104
KushalMalla <kushal.malla@tcs.com>
2023-04-18 16:55:50 +0530

    biuld after input error

--- src/main/java/com/example/SpiralPattern.java
+++ src/main/java/com/example/SpiralPattern.java
@@ -24,7 +24,7 @@ public static void main(String args[])
 {  
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the value of n: ");  
-int n = sc.nextInt();  
+int n = 5;  
 System.out.println();  
 //function calling  
 printPattern(n);  
